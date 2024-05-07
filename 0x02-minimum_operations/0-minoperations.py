dbe276a451994eb6b1d5de6054a7afbd106fe8c4
dietcokechan <dietcokechan@gmail.com>
2024-05-07 22:05:25 +0300

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+def minOperations(n):
+    """ Method that calculates the fewest number of operations needed """
+    if n <= 1:
+        return 0
+    operations = 0
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            operations += i
+            n = n / i
+        else:
+            i += 1
+    return operations

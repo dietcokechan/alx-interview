971ff14ae69557aa6e2a33530549d91939b798e8
dietcokechan <dietcokechan@gmail.com>
2024-06-07 23:38:03 +0300

    star wars characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require("request");
+const filmID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${filmID}`;
+
+request(url, async (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const charID of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(charID, (err, res, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});

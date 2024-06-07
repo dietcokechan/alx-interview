ff65e5672b1a8c41369795a96713df89280a999d
dietcokechan <dietcokechan@gmail.com>
2024-06-07 23:44:48 +0300

    single quotes?

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 const filmID = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${filmID}`;
 
